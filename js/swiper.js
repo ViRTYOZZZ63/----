23be79c444e9ab39e23354d5e6b542fec1a38e9c
ViRTYOZZZ63 <arty2004111@gmail.com>
2024-07-09 15:14:43 +0300

    FIX and new section

---
+++ js/swiper.js
@@ -0,0 +1,67 @@
+// Снизу баганный код(
+const swiper = new Swiper('.features-slider', {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+      centerSlides: true,
+      slidesOffsetBefore: -100,
+      slidesOffsetAfter: 60,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
+
+const swiperSteps = new Swiper(".steps-slider", {
+  speed: 400,
+  slidesPerView: 1 ,
+  navigation: {
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+})
+
+const swiperblog = new Swiper(".blog-slider", {
+  speed: 400,
+  slidesPerView: 1 ,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".blog-button-next",
+    prevEl: ".blog-button-prev",
+  },
+  breakpoints: {
+    1024: {
+      slidesPerView: 2,
+    },
+  },
+});
